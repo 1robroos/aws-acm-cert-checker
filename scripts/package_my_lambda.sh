a4f399b68fa3513fa3eaaa04068b6cf99db2d21b
EC2 Default User <ec2-user@ip-172-31-8-240.eu-central-1.compute.internal>
2021-07-04 14:51:15 +0000

    change bucket and script

--- scripts/package_my_lambda.sh
+++ scripts/package_my_lambda.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cp ../lambda/lambda.py .
 zip -r deployment.zip lambda.py
-aws s3 cp deployment.zip s3://companybucketrobroos/acmcheck/deployment.zip
+aws s3 cp deployment.zip s3://kfsolutions-code-eu-central-1/acmcheck/deployment.zip
 rm lambda.py deployment.zip
